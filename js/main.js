e3f1dd0b854068d809a84d4b3bde54e8a970b019
Casey The Great <casey@caseypitman.com>
2020-01-17 17:19:00 -0600

    version 2.0

--- js/main.js
+++ js/main.js
@@ -1,70 +1,70 @@
-//Rock paper scissors game
-
+//Rock paper scissors 2.0
 
 $(document).ready(function() {//jQuery
+
+  const choices = [
+    "rock",
+    "paper",
+    "scissors"
+  ];
   
-    const tie = "It's a tie. Try again."
-     
-    const choices =[//computer's chooses from here
-      "rock",
-      "paper",
-      "scissors"
-    ];
-      
-    const compare = function(){//function to compare selections of user and computer
-      
-      
-      //retrieves user selection and determines computer's random choice
-       let userSelection = document.getElementById("user-selection").value; 
-       let randomNumber = Math.floor(Math.random() * 3);
-       let computerSelection = choices[randomNumber];
-      
-      if (userSelection==="selected"){ //user neglects to choose
-        alert("Choose rock, paper, or scissors.");
-        return;
-      }//end unselected
-      
-      else if (userSelection === "rock"){//user selects rock
-        if (computerSelection === "scissors"){
-          result = "Rock smashes scissors. You win!";
-        }
-        else if (computerSelection === "paper"){
-          result = "Paper covers rock. You lose.";
-        }
-        else {
-          result = tie;
-        }
-      }//ends user selects rock
-      
-      else if (userSelection === "paper"){//user selects paper
-        if (computerSelection === "rock"){
-          result = "Paper covers rock. You win!";
-        }
-        else if (computerSelection === "scissors"){
-          result = "Scissors cuts paper. You lose.";
-        }
-        else {
-          result = (tie);
-        }
-      }//ends user selects paper
-      
-      else { //user selects scissors
-        if (computerSelection === "paper"){
-          result = "Scissors cuts paper. You win!";
-        }
-        else if (computerSelection === "rock"){
-          result = "Rock smashes scissors. You lose.";
-        }
-        else {
-          result = (tie);
-        }
+  //the computer makes a random choice
+  const computerMakesChoice = function(){
+    let randomNumber = Math.floor(Math.random() * 3.0);
+    let computerChoice = choices[randomNumber];
+    console.log(computerChoice);
+    return computerChoice;
+  }
+  
+  //user chooses rock
+  const chooseRock = function(){
+    let userChoice = choices[0];
+    let computerChoice = computerMakesChoice();
+      if (computerChoice === "paper"){
+        alert("You lose.\nYour opponent chose paper. Paper covers rock.");
+      }else if (computerChoice === "scissors"){
+        alert("YOU WIN!\nYour opponent chose scissors. Rock smashes scissors.");
+      }else {
+        alert("It's a tie!\nYou both chose rock. Shoot again!");
       }
-      //diplays results
-      result = "You chose " + userSelection + ". " + "Computer chose " + computerSelection + ". " + result;
-      alert(result);
-      
-    };//ends compare function
-      
-      $("#play").click(compare);
-      
-    });//ends jQuery
+     return;
+  };
+  
+  //user chooses paper
+  const choosePaper = function(){
+    let userChoice = choices[1];
+    let computerChoice = computerMakesChoice();
+      if (computerChoice === "scissors"){
+        alert("You lose.\nYour opponent chose scissors. Scissors cuts paper.");
+      }else if (computerChoice === "rock"){
+        alert("YOU WIN!\nYour opponent chose rock. Rock covers paper");
+      }else{
+        alert("It's a tie!\nYou both chose paper. Shoot again.");
+      }
+    return;
+  };
+  
+  //use chooses scissors
+  const chooseScissors = function(){
+    let userChoice = choices[2];
+    let computerChoice = computerMakesChoice();
+      if(computerChoice === "rock"){
+        alert("You lose.\nYour opponent chose rock. Rock smashes scissors.");
+      }else if (computerChoice === "paper"){
+        alert("YOU WIN!\nYour opponent chose paper. Scissors cuts paper.");
+      }else{
+        alert("It's a tie!\nYou both chose scissors. Shoot again.")
+      }
+    return;
+  };
+  
+  //user makes choice
+  $("#rock").click(chooseRock);
+  $("#paper").click(choosePaper);
+  $("#scissors").click(chooseScissors);
+  
+  });//ends jQuery
+  
+  
+  
+  
